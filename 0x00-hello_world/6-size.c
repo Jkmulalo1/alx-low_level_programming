874f2f7f00cd67594fda6a5e6466d7b373ea3a49
jkmulalo1 <jkmulalo@gmail.com>
2023-02-16 14:15:48 -0800

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main – A C program that prints the size of various types on the computer
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)
+size of(a);
+}
